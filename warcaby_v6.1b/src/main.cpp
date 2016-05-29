1a355cd28f13194140cc1d708c45236d12ebd203
218490 <jarek.jakub1@gmail.com>
2016-05-30 00:00:03 +0200

    czemu nie bije?

--- warcaby_v6.1b/src/main.cpp
+++ warcaby_v6.1b/src/main.cpp
@@ -2,16 +2,16 @@
 #include "Symulacja.hh"
 using namespace std;
 
-int ProbaB(ruchy plansza,int id){
-  ruchy planszapomo=plansza;
-  return plansza.RuchPrawo(  plansza.wyszukajPionek(id, 'B'));
+int ProbaB(ruchy& plansza,int id){
+  ruchy planszapom=plansza;
+  return planszapom.RuchPrawo(id,'B');
 }
 
 int main(){
   ruchy plansza;
   int ID,flaga;
   string strona;
-  pozycja miejsce;
+   pozycja miejsce;
   plansza.wyswietl();
   plansza.wyswietl(plansza.wyszukajPionek(11, 'B'));
   plansza.RuchLewo(11,'B');
@@ -32,9 +32,7 @@ int main(){
   //  int zysk=RuszCzarne(plansza,0,0,10,strona);
   //cout<<zysk<<endl;
   //int zysk=Symuluj(plansza,ID,strona,flaga,miejsce);
-  cout<<"cos tu nie gra"<<endl;
-  ProbaB(plansza,6);
-  cout<<"cos tu nie gra"<<endl;
+  //ProbaB(plansza,6);
   /*
   if(flaga==0){
   cout<<"ID: "<<ID<<endl;
@@ -50,6 +48,17 @@ int main(){
     if(pionki.empty()){
       cout<<"kolejka pusta"<<endl;
     }*/
-  plansza.wyswietl(plansza.wyszukajPionek(11, 'C'));  
+  plansza.RuchPrawo(6,'B');
+  plansza.wyswietl(plansza.wyszukajPionek(11, 'C'));
+  if(plansza.DostepLewo(11,'C')){
+    cout<<"lewo"<<endl;
+    plansza.RuchLewo(11,'C');
+  }  
+  /*
+  if(plansza.DostepPrawo(11,'C')){
+    cout<<"prawo"<<endl;
+    plansza.RuchPrawo(11,'C');
+    }*/
+  plansza.wyswietl(plansza.wyszukajPionek(11, 'C'));
 
 }
