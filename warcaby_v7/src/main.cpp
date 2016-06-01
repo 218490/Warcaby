39079fe0275ee4cd0d0908e0610a2ed49dd6fb89
218490 <jarek.jakub1@gmail.com>
2016-06-01 14:56:28 +0200

    blad w: DostepneCzarne() i DostepLewo()

---
+++ warcaby_v7/src/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include "Symulacja.hh"
+using namespace std;
+
+int ProbaB(ruchy& plansza,int id){
+  ruchy planszapom=plansza;
+  return planszapom.RuchPrawo(id,'B');
+}
+
+int main(){
+  ruchy plansza;
+  int ID,flaga;
+  string strona;
+   pozycja miejsce;
+  plansza.wyswietl();
+  plansza.wyswietl(plansza.wyszukajPionek(11, 'B'));
+  plansza.RuchLewo(11,'B');
+  plansza.wyswietl();
+  plansza.wyswietl(plansza.wyszukajPionek(12, 'B'));
+  plansza.RuchPrawo(12,'B');
+  plansza.wyswietl();
+  plansza.wyswietl(plansza.wyszukajPionek(10, 'C'));
+  plansza.RuchLewo(10,'C');
+  plansza.wyswietl();
+  plansza.wyswietl(plansza.wyszukajPionek(11, 'C'));
+  plansza.RuchPrawo(11,'C');
+  plansza.wyswietl();
+  plansza.wyswietl(plansza.wyszukajPionek(10, 'C'));
+  plansza.RuchPrawo(10,'C');
+  plansza.wyswietl();
+
+  /*
+  int zysk=RuszCzarne(plansza,0,0,7,strona);
+  cout<<"Zysk:"<<zysk<<endl;
+  cout<<"Strona:"<<strona<<endl;
+*/
+
+  //cout<<zysk<<endl;
+  //int zysk=Symuluj(plansza,ID,strona,flaga,miejsce);
+  //ProbaB(plansza,6);
+  /*
+  if(flaga==0){
+  cout<<"ID: "<<ID<<endl;
+  cout<<"strona: "<<strona<<endl;
+  cout<<"zysk: "<<zysk<<endl;
+  }*/
+  queue<int> pionki=plansza.DostepneCzarne();
+  int rozmiartab=pionki.size();
+    for(int i=0;i<rozmiartab;i++){
+    cout<<pionki.front()<<"  ";
+    pionki.pop();
+  }
+    if(pionki.empty()){
+      cout<<"kolejka pusta"<<endl;
+      }
+  plansza.wyswietl(plansza.wyszukajPionek(7, 'C'));
+  if(plansza.DostepLewo(7,'C')){
+    cout<<"moze isc w lewo pionkiem 7"<<endl;
+  }
+  else cout<<"NIE moze isc w lewo pionkiem 7"<<endl;
+
+/*
+  plansza.RuchPrawo(6,'B');
+  plansza.wyswietl(plansza.wyszukajPionek(11, 'C'));
+  if(plansza.DostepLewo(11,'C')){
+    cout<<"lewo"<<endl;
+    plansza.RuchLewo(11,'C');
+  }  */
+  //plansza.wyswietl(plansza.wyszukajPionek(7, 'C'));
+
+}
